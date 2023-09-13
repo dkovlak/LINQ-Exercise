c634e593ac340a0ef70f46d2e25f7f0bd1697c4f
David Kovlak <kovlakdavid4@gmail.com>
2023-09-13 09:19:27 -0700

    Linq

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>();
+
+            videoGames.Add("Tanki Online");
+            videoGames.Add("The Legend of Zelda: Breath of the Wild");
+            videoGames.Add("Red Dead Redemption 2");
+            videoGames.Add("Cyberpunk 2077");
+            videoGames.Add("Super Mario Odyssey");
+            videoGames.Add("The Witcher 3: Wild Hunt");
+            videoGames.Add("Minecraft");
+            videoGames.Add("Fortnite");
+            videoGames.Add("Overwatch");
+            videoGames.Add("Call of Duty: Warzone");
+
+            var length = videoGames.OrderBy(x => x.Length);
+
+            int count = 0;
+            Console.WriteLine("~~~~~ List of Video Games ~~~~~");
+            foreach (string game in length)
+            {
+                count++;
+                Console.WriteLine($"Game #{count}: {game}");
+            }
+
+
+
         }
     }
 }
